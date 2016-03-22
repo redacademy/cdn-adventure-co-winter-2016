8565b4ec6781d6dc0e250b51ae2acf7855c092fd
Jillian Forsythe <j.forsythe8@gmail.com>
2016-03-22 15:04:52 -0700

    Style footer content

--- wp-content/themes/canadianadventureco/footer.php
+++ wp-content/themes/canadianadventureco/footer.php
@@ -12,19 +12,21 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
 					<h3>Get in Touch</h3>
-					<a href="mailto:info@cacltd.com">Email: info@cacltd.com</a>
-					<a href="tel:2508354516">Phone: 250.835.4516</a>
+					<div class="contact-info">
+						<a href="mailto:info@cacltd.com">Email: info@cacltd.com</a>
+						<a href="tel:2508354516">Phone: 250.835.4516</a>
+					</div><!-- contact-info -->
 					<div class="social-icons">
 						<a href="https://twitter.com/CanAdvCo">
-							<i class="fa fa-border fa-twitter"></i>
+							<i class="fa fa-border fa-fw fa-twitter"></i>
 						</a>
 						<a href="https://www.facebook.com/CanadianAdventureCompanyLtd">
-							<i class="fa fa-border fa-facebook"></i>
+							<i class="fa fa-border fa-fw fa-facebook"></i>
 						</a>
 						<a href="https://www.instagram.com/canadianadventurecompany">
-							<i class="fa fa-border fa-instagram"></i>
+							<i class="fa fa-border fa-fw fa-instagram"></i>
 						</a>
-					</div>
+					</div><!-- .social-icons -->
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
