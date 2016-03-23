efa4968c9668e9731e012c9261bce7388579cf80
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-03-23 16:19:25 -0700

    Add Winter Packages Files

---
+++ wp-content/themes/canadianadventureco/winter-packages.php
@@ -0,0 +1,134 @@
+<?php
+/**
+* Template Name: Winter-Packages Page
+*
+* @package RED_Starter_Theme
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<!-- This is the winter-packages hero banner -->
+		<section>
+			<div class="banner winter-packages-hero">
+				<div class="banner-text">
+					Winter Packages
+				</div>
+			</div>
+		</section>
+
+      <!-- This is the "our winter season" section -->
+      <section class="banner column">
+         <div class="main-title">
+           <h1>Our Winter Season is from November to April</h1>
+         </div>
+         <div class="single-column">
+            <p>Canadian Adventure Company offers highly customized
+              packages for guests, so that they can enjoy their perfect
+              getaway at the Mallard Lodge. Due to the often extreme nature
+              of the winter season, all of our winter season packages are
+              guided by experts.</p>
+         </div>
+      </section>
+
+      <!-- This is the "winter activities" section -->
+      <section class="banner column">
+         <div class="">
+            <h2>Winter Activities</h2>
+         </div>
+         <div class="">
+            <img src="" alt="Ski Icon">
+            <img src="" alt="Snowboard Icon">
+            <img src="" alt="Snowshoe Icon">
+         </div>
+
+         <div>
+           <p>When you arrive for your winter adventure, spend your days skiing,
+             snowboarding, snowshoeing or just relaxing in the lodge. With your guide,
+             you’ll experience some of the most remote regions of British Columbia and
+             ski down slopes that have never been skiied before.</p>
+        </div>
+		</section>
+
+    <!-- This is the guided sub banner -->
+    <section>
+      <div class="banner guided-hero">
+        <div class="banner-text">
+          Guided By Experts
+        </div>
+      </div>
+    </section>
+
+    <!-- This is the "safety priority" section -->
+    <section class="banner column">
+       <div class="main-title">
+         <h2>Safety is our first priority, and our guides are experts in the backcountry.</h2>
+       </div>
+       <div class="single-column">
+          <p>Safety is our first priority, and our guides are experts with years of experience
+            in the backcountry... Etc etc. Lorem Ipsum is simply dummy text of the printing and
+             typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+             since the 1500s, when an unknown printer took a galley of type and scrambled it to
+             make a type specimen book.</p>
+       </div>
+    </section>
+
+    <!-- This is the meals sub banner -->
+    <section>
+      <div class="banner meals-hero">
+        <div class="banner-text">
+          Meals
+        </div>
+      </div>
+    </section>
+
+
+    <!-- This is the "cooking" section -->
+    <section class="banner column">
+       <div class="main-title">
+         <h2>Leave the cooking to us with a fully-catered stay, or choose to bring your own food.</h2>
+       </div>
+       <div class="single-column">
+          <p>As part of your experience with us at the lodge, we will provide and
+             cook the food for you and your guests. If you have dietary restrictions or needs,
+              let us know and we’ll cater the meals to you and your group. If you decide you want
+              to bring your own food and cook your own meals, you can do that too! Just let us
+              know what works best for you.</p>
+       </div>
+    </section>
+
+    <!-- This is the small-groups sub banner -->
+    <section>
+      <div class="banner small-groups-hero">
+        <div class="banner-text">
+          Small Groups
+        </div>
+      </div>
+    </section>
+
+    <!-- This is the "maximum groups" section -->
+    <section class="banner column">
+       <div class="main-title">
+         <h2>With maximum groups of 9, guests will experience an intimate experience at the lodge.</h2>
+       </div>
+       <div class="single-column">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+       </div>
+    </section>
+<!-- This is the "trip-estimate" section -->
+    <section>
+      <button class="button">
+        Get a Trip Estimate
+      </button>
+    </section>
+
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
