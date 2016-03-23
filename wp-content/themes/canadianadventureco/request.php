853c3cc75a0abbdc5a12cac32507c478ec75b503
Cameron Fuller <franksjacket@yahoo.com>
2016-03-23 11:51:17 -0700

    Built basis for angular app.

---
+++ wp-content/themes/canadianadventureco/request.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Request Page
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+	<div id="primary" class="content-area" ng-app="BookingApp" ng-controller="BookingAppCtrl">
+		<main id="main" class="site-main" role="main">
+			<div class="">
+				<button class="button" name="button">Choose</button>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
