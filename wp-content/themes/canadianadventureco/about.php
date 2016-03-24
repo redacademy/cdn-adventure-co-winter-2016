c880bf23abe85914ecd0e8385b3a6a5710d821e1
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-03-24 14:24:41 -0700

    Headings Styling

--- wp-content/themes/canadianadventureco/about.php
+++ wp-content/themes/canadianadventureco/about.php
@@ -14,24 +14,23 @@ get_header(); ?>
 		<section>
 			<div class="banner about-hero">
 				<div class="banner-text">
-					Who we are 
+					Who we are
 			</div>
 		</section>
 
       <!-- This is the "get to know the mcmanus family" section -->
-      <section class="banner column">
-         <div class="main-title">
-           <h1>Get to know the McManus Family</h1>
-         </div>
+      <section>
+				<div class="banner column-what">
+	         <div class="main-title">
+	           <h1>Get to know the McManus Family</h1>
+	         </div>
+			 </div>
          <div class="single-column">
-            <?php //put the info text here ?>
             <p>Lorem ipsum dolor sit amet, at eos veri conceptam, duis equidem eu nam, usu cu errem
               splendide contentiones. At vel vidit efficiendi. Case justo dicta ea vix, cu dolor iudicabit
               eos, nam tation postulant suscipiantur ei. Modo errem veritus at eam. Quo cibo phaedrum vituperata
               ne, per ad probo eripuit, mea te prompta facilis eleifend. Nam debitis lucilius deseruisse ne.
-              Qui deserunt philosophia ea, aperiri voluptatum ei nec. Ne offendit ocurreret voluptatibus eos,
-              soleat phaedrum ex usu. Ut melius integre petentium mel, his stet veniam invenire eu.
-              Vim causae euismod liberavisse id, ei usu facer recusabo. Eum et diceret lobortis.</p>
+              Qui deserunt philosophia ea, aperiri voluptatum ei nec. </p>
          </div>
       </section>
 
@@ -45,20 +44,21 @@ get_header(); ?>
 		</section>
 
       <!-- This is the "why we built the mallard lodge" section -->
-      <section class="banner column">
-         <div class="">
-            <h2>Why we built the Mallard Lodge</h2>
-         </div>
-         <div class="">
-            <?php //put the info text here ?>
-            <p>Lorem ipsum dolor sit amet, at eos veri conceptam, duis equidem eu nam, usu cu errem
-              splendide contentiones. At vel vidit efficiendi. Case justo dicta ea vix, cu dolor iudicabit
-              eos, nam tation postulant suscipiantur ei. Modo errem veritus at eam. Quo cibo phaedrum vituperata
-              ne, per ad probo eripuit, mea te prompta facilis eleifend. Nam debitis lucilius deseruisse ne.
-              Qui deserunt philosophia ea, aperiri voluptatum ei nec. Ne offendit ocurreret voluptatibus eos,
-              soleat phaedrum ex usu. Ut melius integre petentium mel, his stet veniam invenire eu.
-              Vim causae euismod liberavisse id, ei usu facer recusabo. Eum et diceret lobortis.</p>
-         </div>
+      <section>
+				<div class="banner column-what">
+	         <div class="main-title">
+	            <h2>Why we built the Mallard Lodge</h2>
+	         </div>
+	         <div class="single-column">
+	            <p>Lorem ipsum dolor sit amet, at eos veri conceptam, duis equidem eu nam, usu cu errem
+	              splendide contentiones. At vel vidit efficiendi. Case justo dicta ea vix, cu dolor iudicabit
+	              eos, nam tation postulant suscipiantur ei. Modo errem veritus at eam. Quo cibo phaedrum vituperata
+	              ne, per ad probo eripuit, mea te prompta facilis eleifend. Nam debitis lucilius deseruisse ne.
+	              Qui deserunt philosophia ea, aperiri voluptatum ei nec. Ne offendit ocurreret voluptatibus eos,
+	              soleat phaedrum ex usu. Ut melius integre petentium mel, his stet veniam invenire eu.
+	              Vim causae euismod liberavisse id, ei usu facer recusabo. Eum et diceret lobortis.</p>
+	         </div>
+			 </div>
 
 			<button class="button">
 				Explore the lodge
