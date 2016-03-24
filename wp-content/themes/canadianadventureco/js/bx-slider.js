c1465f46d0f3208d8753a127139072142c1a5c0a
Jillian Forsythe <j.forsythe8@gmail.com>
2016-03-24 14:13:08 -0700

    toggle mobile menu

--- wp-content/themes/canadianadventureco/js/bx-slider.js
+++ wp-content/themes/canadianadventureco/js/bx-slider.js
@@ -1,6 +1,15 @@
 jQuery(document).ready(function(){
+
   jQuery('.bxslider').bxSlider({
     pagerCustom: '#bx-pager',
     slideMargin: 10,
+  });
+
+  jQuery('.menu-button').on('click', function() {
+    jQuery('.main-navigation').toggle().addClass('mobile-menu');
+
+    jQuery('#primary-menu').toggle();
+    jQuery('.site-branding').toggle();
+
   });
 });
