71a4729cd06d2d9e15e9f2287117b73f1d8ad379
Jillian Forsythe <j.forsythe8@gmail.com>
2016-03-27 13:53:01 -0700

    Add and style submission page

---
+++ wp-content/themes/canadianadventureco/submission.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Template Name: Submission Page
+*
+* @package RED_Starter_Theme
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+
+    <section class="submission container title">
+
+      <h1>We've Recieved Your Submission!</h1>
+
+      <p>
+        Thank you for your request. We will contact you shortly.
+      </p>
+
+      <a href="<?php echo esc_url(home_url('/')); ?>">
+        <button type="button" class="button">
+          Go Back Home
+        </button>
+      </a>
+
+    </section>
+    
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
