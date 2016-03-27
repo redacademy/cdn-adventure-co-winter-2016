440b5002fb367c8fb2faa26ab8248a30a645664f
Jillian Forsythe <j.forsythe8@gmail.com>
2016-03-26 18:08:57 -0700

    mobile navigation fixes

--- wp-content/themes/canadianadventureco/js/bx-slider.js
+++ wp-content/themes/canadianadventureco/js/bx-slider.js
@@ -1,9 +1,13 @@
 jQuery(document).ready(function(){
 
+//lodge photo gallery
+
   jQuery('.bxslider').bxSlider({
     pagerCustom: '#bx-pager',
     slideMargin: 10,
   });
+
+//mobile menu function
 
   jQuery('.menu-button').on('click', function() {
     jQuery('.mobile-navigation').toggle().toggleClass('mobile-menu');
