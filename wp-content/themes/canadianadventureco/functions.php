7116b875481de0c7ed831b7bd9dc318da5a77e40
Jillian Forsythe <j.forsythe8@gmail.com>
2016-03-28 13:40:56 -0700

    Fix desktop header

--- wp-content/themes/canadianadventureco/functions.php
+++ wp-content/themes/canadianadventureco/functions.php
@@ -90,12 +90,18 @@ function cdn_adventure_scripts() {
 	wp_enqueue_style( 'font-awesome-cdn', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), '4.4.0');
 
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+
 	wp_enqueue_script( 'jquery', get_template_directory_uri() . 'https://code.jquery.com/jquery-2.2.2.min.js');
+
 	wp_enqueue_script( 'angular', get_template_directory_uri() . '/build/js/angular.min.js');
+
 	wp_enqueue_script( 'ui-router', get_template_directory_uri() . '/build/js/angular.ui-router.min.js');
+
 	wp_enqueue_script( 'BookingApp', get_template_directory_uri() . '/js/booking-app.js');
+
 	wp_enqueue_script( 'bxslider', get_template_directory_uri() . '/js/jquery.bxslider/jquery.bxslider.min.js', array('jquery'));
-	wp_enqueue_script( 'main', get_template_directory_uri(). 'build/js/main.min.js');
+	
+	wp_enqueue_script( 'main', get_template_directory_uri(). '/build/js/main.min.js');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
