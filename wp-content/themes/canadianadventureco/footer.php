f08f49fa4447a7c320248b3988f88356dbfc42a5
Jillian Forsythe <j.forsythe8@gmail.com>
2016-04-06 11:28:52 -0800

    Add target=_blank to social links in footer

--- wp-content/themes/canadianadventureco/footer.php
+++ wp-content/themes/canadianadventureco/footer.php
@@ -17,13 +17,13 @@
 						<a href="tel:2508354516" target="_blank">Phone: 250.835.4516</a>
 					</div><!-- contact-info -->
 					<div class="social-icons">
-						<a href="https://twitter.com/CanAdvCo">
+						<a href="https://twitter.com/CanAdvCo" target="_blank">
 							<i class="fa fa-border fa-fw fa-twitter"></i>
 						</a>
-						<a href="https://www.facebook.com/CanadianAdventureCompanyLtd">
+						<a href="https://www.facebook.com/CanadianAdventureCompanyLtd" target="_blank">
 							<i class="fa fa-border fa-fw fa-facebook"></i>
 						</a>
-						<a href="https://www.instagram.com/canadianadventurecompany">
+						<a href="https://www.instagram.com/canadianadventurecompany" target="_blank">
 							<i class="fa fa-border fa-fw fa-instagram"></i>
 						</a>
 					</div><!-- .social-icons -->
